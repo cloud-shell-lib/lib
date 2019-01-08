1f53ebe2622ca968d0d17a9fe7dd0cc0191353f2
xaoxuu <xaoxuu@gmail.com>
2019-01-08 15:14:59 +0800

    update

--- download.sh
+++ download.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 param1_repo_name=$1
-param2_branch=$2
+param2_version=$2
 param3_client_name=$3
-param4_download_path=$4
+param4_cache_path=$4
 
 function func_download() {
-	case $param2_branch in
-		''|'*') param2_branch='master' ;;
+	case $param2_version in
+		''|'*') param2_version='master' ;;
 		*) ;;
 	esac
 	case $param3_client_name in
 		''|'*') param3_client_name=${param1_repo_name} ;;
 		*) ;;
 	esac
-	source_url=https://raw.githubusercontent.com/xaoxuu/${param1_repo_name}/${param2_branch}/${param3_client_name}
-	case $param4_download_path in
-		'') param4_download_path=$HOME/Downloads/ ;;
+	source_url=https://cdn.jsdelivr.net/gh/xaoxuu/${param1_repo_name}@${param2_version}/${param3_client_name}
+	case $param4_cache_path in
+		'') param4_cache_path=$HOME/Downloads/ ;;
 		*) ;;
 	esac
-	if [ ! -d ${param4_download_path} ];then
-		mkdir ${param4_download_path}
+	if [ ! -d ${param4_cache_path} ];then
+		mkdir ${param4_cache_path}
 	fi
 	echo "> 正在下载: ${source_url}"
-	curl -f -o ${param4_download_path}/${param3_client_name} ${source_url} -# &&
-	echo "> 下载成功，存放路径为: ${param4_download_path%/*}/${param3_client_name}"
+	curl -f -o ${param4_cache_path}/${param3_client_name} ${source_url} -# &&
+	echo "> 下载成功，存放路径为: ${param4_cache_path%/*}/${param3_client_name}"
 }
 
 
