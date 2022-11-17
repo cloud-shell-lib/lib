a9c6fe82fb14da7bca1750673ea5a3aa39cd33fc
xaoxuu <git@xaoxuu.com>
2022-11-17 22:06:03 +0800

    index

--- index.sh
+++ index.sh
@@ -1,45 +1,23 @@
 #!/bin/bash
 
-# 下载什么
 NAME=$1
-# 哪个版本
 BRANCHE=$2
 
-# 其它参数
-P3=$3
-P4=$4
-
-function on_fail() {
-	NAME=""
-	BRANCHE=""
-	printf "\n> \033[31m下载失败！\033[0m\n\n"
-}
-
-function start() {
-	if [ "$BRANCHE" == "" ];then
-		BRANCHE='main'
-	fi
-	source_url=https://sh.xaox.cc/run/${NAME}/${BRANCHE}/index.sh
-	printf '\n> 正在请求下载 %s\n' ${source_url}
-	curl -s ${source_url} | sh -s ${P3} ${P4} || on_fail
-}
-
-function check_param() {
-		case $NAME in
-			'')
-				clear
-				printf "可安装的脚本:\n"
-				printf "  \033[1m\033[32m%s\033[0m %s (docs: %s) \n" 'blog   ' '' 'https://xaoxuu.com/wiki/cloud-shell/blog/'
-				printf "  \033[1m\033[32m%s\033[0m %s (docs: %s) \n" 'vimcn  ' '' 'https://xaoxuu.com/wiki/cloud-shell/vimcn/'
-				printf "  \033[1m\033[32m%s\033[0m %s (docs: %s) \n" 'tag    ' '' 'https://xaoxuu.com/wiki/cloud-shell/tag/'
-				printf "  \033[1m\033[32m%s\033[0m %s (docs: %s) \n" 'podspec' '' 'https://xaoxuu.com/wiki/cloud-shell/podspec/'
-				printf "\n"
-				printf "默认安装最新版本，可以在脚本后面加上版本号安装指定版本或分支，用空格隔开。例如:\n"
-				printf "curl -s https://sh.xaox.cc/install | bash -s blog 3.0.0\n"
-				printf "\n"
-			;;
-			*) start ;;
-		esac
-}
-
-check_param
+case $NAME in
+  '')
+    clear
+    printf "欢迎使用 cloud-shell 轻量级云端脚本库！\n"
+    printf "查看可安装的脚本: https://xaoxuu.com/wiki/cloud-shell/\n"
+    printf "默认安装最新版本，可以在脚本后面加上版本号安装指定版本或分支，用空格隔开。例如:\n"
+    printf "> curl -s https://sh.xaox.cc/install | bash -s \033[32m%s\033[0m \033[34m%s\033[0m\n" "blog" "3.0.0"
+    printf "\n"
+  ;;
+  *) 
+    if [ "$BRANCHE" == "" ];then
+      BRANCHE='main'
+    fi
+    source_url=https://sh.xaox.cc/run/${NAME}/${BRANCHE}/index.sh
+    printf '\n> 正在请求下载 %s\n' ${source_url}
+    curl -s ${source_url} | sh -s ${BRANCHE} || printf "\n> \033[31m下载失败！\033[0m\n\n"
+  ;;
+esac
